244cb21e7c3350b741a70dc1aaeb700dc9630433
fankaihong <712024864@qq.com>
2020-05-05 10:32:25 +0800

    test

--- app/src/App.vue
+++ app/src/App.vue
@@ -18,6 +18,12 @@
 									<el-menu-item index="1-1">动态温湿度</el-menu-item>
 									<el-menu-item index="1-2">选项2</el-menu-item>
 									<el-menu-item index="1-3">选项3</el-menu-item>
+									<el-submenu index="1-4">
+										<template slot="title">首页</template>
+										<el-menu-item index="a">动态温湿度</el-menu-item>
+										<el-menu-item index="v">选项2</el-menu-item>
+										<el-menu-item index="c">选项3</el-menu-item>
+									</el-submenu>
 								</el-submenu>
 								<el-submenu index="2">
 									<template slot="title">报表管理</template>
@@ -25,14 +31,10 @@
 									<el-menu-item index="2-2">仓库温湿度报表</el-menu-item>
 									<el-menu-item index="2-3">用户管理报表</el-menu-item>
 								</el-submenu>
-								<el-submenu index="3">
+								<el-submenu index="User">
 									<template slot="title">用户管理</template>
-									<el-menu-item index="3-1">
-										<router-link to="/userList">用户查询</router-link>
-									</el-menu-item>
-									<el-menu-item index="3-2">
-										<router-link to="/userAdd">用户新增</router-link>
-									</el-menu-item>
+									<el-menu-item index="UserList">用户查询</el-menu-item>
+									<el-menu-item index="UserAdd">用户新增</el-menu-item>
 								</el-submenu>
 								<el-submenu index="4">
 									<template slot="title">我的工作台</template>
@@ -51,19 +53,83 @@
 				</el-row>
 			</el-header>
 			<el-main>
-				<TabView></TabView>
+				<el-tabs v-model="currentTab" @tab-click="changeView" type="card" closable @tab-remove="removeTab">
+					<el-tab-pane v-for="(item, index) in editableTabs" :key="item.name" v-if="item.isExist" :label="item.title" :name="item.name" />
+					<keep-alive>
+						<component v-bind:is="currentTabComponent"></component>
+					</keep-alive>
+				</el-tabs>
 			</el-main>
 		</el-container>
 	</div>
 </template>
 
 <script>
-	import TabView from "@/components/TabView.vue";
-
+	import UserList from "@/components/UserList.vue";
+	import UserAdd from "@/components/UserAdd.vue";
+	
+	const menuList = [{
+		title: '用户查询',
+		name: 'UserList',
+		isExist: false
+	}, {
+		title: '用户新增',
+		name: 'UserAdd',
+		isExist: false
+	}];
+	
 	export default {
 		name: "App",
 		components: {
-			TabView
+			UserList,
+			UserAdd
+		},
+		data() {
+			return {
+				currentTab: "UserList",
+				editableTabsValue: 'UserList',
+				editableTabs: menuList
+			}
+		},
+		computed: {
+			currentTabComponent: function() {
+				return this.currentTab;
+			}
+		},
+		watch: {
+			$route(menu) {
+				console.dir(menu);
+				this.addTab(menu.name);
+			}
+		},
+		methods: {
+			handleSelect(key, keyPath) {
+				this.addTab(key);
+			},
+			changeView(tab) {
+				this.currentTab = tab.name;
+			},
+			addTab(targetName) {
+				this.editableTabs.forEach((tab, index) => {
+					if (tab.name === targetName) {
+						if (!tab.isExist) {
+							tab.isExist = true;
+						}
+						this.editableTabsValue = this.currentTab;
+						this.currentTab = tab.name;
+					}
+				});
+			},
+			removeTab(targetName) {
+				this.editableTabs.forEach((tab, index) => {
+					if (tab.name === targetName) {
+						if (tab.isExist) {
+							tab.isExist = false;
+							this.currentTab = this.editableTabsValue;
+						}
+					}
+				});
+			}
 		}
 	}
 </script>
